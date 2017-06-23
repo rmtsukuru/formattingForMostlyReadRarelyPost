de454c17129f0110e05ccce09614724eca6693d8
rmtsukuru <samcollegeemail@gmail.com>
2017-06-23 01:00:58 -0400

    Updated formatting.

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ $(function() {
         var text = $(this).val();
         var paragraphs = text.split('\n\n');
         $('#output').html('');
+        $('#output').append('<p>****<br />****</p>');
         $.each(paragraphs, function(i, paragraph) {
-            var contents = '<p>';
+            var contents = '';
             var lines = paragraph.split('\n');
             $.each(lines, function(j, line) {
-                contents += '&gt; ' + line + '<br />';
+                contents += '<p>&gt; ' + line + '</p>';
             });
-            contents += '</p>';
-            $('#output').append(contents);
+            $('#output').append(contents + '<p>****</p>');
         });
+        $('#output p:last-child').html('<p>****<br />****</p>');
     });
 });
